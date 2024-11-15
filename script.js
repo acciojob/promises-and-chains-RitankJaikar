9e6895901393a509921f686bfb191b9c7b71fae3
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-15 21:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let name= document.querySelector("#name");
+let age= document.querySelector("#age");
+let form= document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age.value>=18) {
+				resolve(`Welcome, ${name.value}. You can vote.`);
+			}
+			else {
+				reject(`Oh sorry ${name.value}. You aren't old enough.`);
+			}
+		}, 4000);
+	})
+	.then(res => {
+		console.log(res);
+		alert(res);
+	})
+	.catch(err => {
+		console.log(err);
+		alert(err);
+	})
+})
+
+
+
